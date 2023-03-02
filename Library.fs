930f676996a21e043a25576440503438da73dd84
hale2024 <71109353+hale2024@users.noreply.github.com>
2023-03-02 17:46:10 +0900

    Add files via upload

---
+++ Library.fs
@@ -0,0 +1,16 @@
+﻿module NativeLibrary
+open System.Runtime.InteropServices
+open System
+
+[<UnmanagedCallersOnly(EntryPoint = "add")>]
+let Add (a: int) (b: int) : int =
+    a + b
+    
+[<UnmanagedCallersOnly(EntryPoint = "write_line")>]
+let WriteLine (pString: IntPtr) : int =
+    try
+        let str = Marshal.PtrToStringAnsi(pString)
+        Console.WriteLine(str)
+        0
+    with
+    | _ -> -1
